f6767423641def310f4ecde8bbe61d557d99beb9
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-02-21 11:35:27 +0600

    forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile  # Profile মডেলটি ইমপোর্ট করা হয়েছে
+from .models import Profile 
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -11,11 +11,10 @@ class UserUpdateForm(forms.ModelForm):
 
 
 class RegistrationForm(UserCreationForm):
-    # RegistrationForm-এ নতুন user_type ফিল্ডটি যোগ করা হয়েছে
     user_type = forms.ChoiceField(
-        choices=Profile.USER_TYPES,  # এখানে Profile.USER_TYPES ব্যবহার করা হয়েছে
+        choices=Profile.USER_TYPES,  
         required=True,
-        label="ইউজারের ধরণ",
+        label="User type",
     )
     email = forms.EmailField(required=True)
 
@@ -24,14 +23,12 @@ class RegistrationForm(UserCreationForm):
         fields = ['username', 'email']
 
     def save(self, commit=True):
-        # UserCreationForm এর ডিফল্ট save() মেথডটি override করা হয়েছে
-        # প্রথমে User মডেলটি সেভ করা হয়
         user = super().save(commit=False)
         user.email = self.cleaned_data.get('email')
         if commit:
             user.save()
 
-        # User সেভ করার পর Profile মডেলটি তৈরি বা আপডেট করা হয়
+
         user_profile = Profile.objects.create(user=user)
         user_profile.user_type = self.cleaned_data.get('user_type')
         if commit:
