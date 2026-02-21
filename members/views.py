f73201617bc4aff86c0c15c814bd60b0ef1fdfd0
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-02-21 11:49:56 +0600

    views.py

--- members/views.py
+++ members/views.py
@@ -5,22 +5,13 @@ from .models import Course, Teacher, SiteSettings, SuccessStory, SlideshowImage
 
 
 def home_page(request):
-    """
-    Handles the homepage view, retrieving all necessary data for display.
-    This includes popular courses, teachers, site settings, success stories, and slideshow images.
-    """
-    # Fetching courses (top 6)
+
     courses = Course.objects.all()[:6]
-    # Fetching teachers (top 4)
     teachers = Teacher.objects.all()[:4]
-    # Fetching site settings
     site_settings = SiteSettings.objects.first()
-    # Fetching all success stories
     success_stories = SuccessStory.objects.all()
-    # Fetching all slideshow images
     slideshow_images = SlideshowImage.objects.all()
 
-    # Creating the context dictionary to pass data to the template
     context = {
         'courses': courses,
         'teachers': teachers,
@@ -29,7 +20,6 @@ def home_page(request):
         'slideshow_images': slideshow_images,
     }
 
-    # Rendering the homepage template with the prepared context
     return render(request, 'members/home.html', context)
 
 
