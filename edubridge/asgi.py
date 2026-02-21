917bae1eaf7fca077cab23a0697f4105b028bdf6
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-02-21 12:02:14 +0600

    asgi.py

--- edubridge/asgi.py
+++ edubridge/asgi.py
@@ -1,11 +1,5 @@
-"""
-ASGI config for edubridge project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
 
 import os
 
