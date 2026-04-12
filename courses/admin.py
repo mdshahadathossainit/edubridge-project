1878692d78c3797f8e59d03f84beafc32a8a7ae3
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-04-12 13:52:16 +0600

    Update admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,14 +1,13 @@
-# courses/admin.py
-
 from django.contrib import admin
 from .models import Course, Enrollment  
 
-
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'instructor')
+    list_select_related = ('instructor',)
 
 @admin.register(Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
     list_display = ('student', 'course', 'enrolled_at')
     list_filter = ('course', 'enrolled_at')
     list_select_related = ('student', 'course')
-
-
