27f42dbfef0e0f510cc2082b71e8801635440491
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-04-12 13:30:09 +0600

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,18 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile 
-
-
-class UserUpdateForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'email']
-
+from .models import Profile
 
 class RegistrationForm(UserCreationForm):
     user_type = forms.ChoiceField(
-        choices=Profile.USER_TYPES,  
+        choices=Profile.USER_TYPES,
         required=True,
         label="User type",
     )
@@ -27,17 +20,12 @@ class RegistrationForm(UserCreationForm):
         user.email = self.cleaned_data.get('email')
         if commit:
             user.save()
-
-
-        user_profile = Profile.objects.create(user=user)
-        user_profile.user_type = self.cleaned_data.get('user_type')
-        if commit:
-            user_profile.save()
-
+            profile, created = Profile.objects.get_or_create(user=user)
+            profile.user_type = self.cleaned_data.get('user_type')
+            profile.save()
         return user
-
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'first_name', 'last_name']
