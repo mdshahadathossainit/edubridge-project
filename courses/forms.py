bb3f2a4185b55d06fe88b75bd54f7815543a3580
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-04-13 23:35:02 +0600

    Update forms.py

--- courses/forms.py
+++ courses/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from .models import Course
+from .models import Course, Homework
 
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = ['title', 'description', 'instructor', 'duration', 'price', 'image']
+
+class HomeworkForm(forms.ModelForm):
+    class Meta:
+        model = Homework
+        fields = ['course', 'description', 'due_date']
+        widgets = {
+            'due_date': forms.DateInput(attrs={'type': 'date'}),
+        }
